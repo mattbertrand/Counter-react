e668c77ffec1fad889f2141812fd79c9ff206a26
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-05-14 14:56:18 -0700

    created a form to add a new todo and delete it

--- src/features/counter/todo/Todo.js
+++ src/features/counter/todo/Todo.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+    create,
+    edit,
+    toggleComplete,
+    remove
+} from './todoSlice'
 
 const Todo = () => {
+    const [inputText, setInputText] = useState('')
+    const dispatch = useDispatch()
+    const todos = useSelector(state => state.todos)
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        dispatch(create(inputText))
+    }
+
+    const handleDelete = (id) => () => {
+        dispatch(remove(id))
+    }
+
     return (
         <div>
-         Todo   
+         <form onSubmit={handleSubmit}>
+             <input onChange={e => setInputText(e.target.value)} value={inputText}></input>
+             <button type="submit">Create New</button>
+        </form> 
+        {todos.map(todo => (
+            <div key={todo.id}>{todo.description} {todo.isComplete ? "DONE" : ""}
+            <button onClick={handleDelete(todo.id)}>Delete</button>
+            </div>
+            
+        ))} 
         </div>
     )
 }
